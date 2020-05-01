ddbaf33a8cecb8448a481fef873f813bc74615ac
tmzkt21 <ehswn67@naver.com>
2020-05-01 14:48:50 +0900

    14:48

--- occamsrazor-1/src/main/java/com/occamsrazor/web/admin/AdminService.java
+++ occamsrazor-1/src/main/java/com/occamsrazor/web/admin/AdminService.java
@@ -2,6 +2,9 @@ package com.occamsrazor.web.admin;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public interface AdminService {
 
 	public void register(Admin admin);
